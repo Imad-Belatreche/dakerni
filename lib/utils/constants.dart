0c1df14b58fcd44fd81796f3104e3524521243a4
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2026-04-29 00:28:39 +0100

    Use pure black instead of deep black

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 const _cyberRed = Color.fromARGB(255, 171, 0, 0);
-const _deepBlack = Color.fromARGB(255, 20, 0, 0);
+const _pureBlack = Colors.black;
 const _pureWhite = Colors.white;
 
 final colorScheme = ColorScheme.fromSeed(
   primary: _cyberRed,
   onPrimary: _pureWhite,
   secondary: _pureWhite,
-  onSecondary: _deepBlack,
-  surface: _deepBlack,
+  onSecondary: _pureBlack,
+  surface: _pureBlack,
   onSurface: _pureWhite,
   seedColor: const Color.fromARGB(255, 61, 0, 1),
   brightness: Brightness.dark,
